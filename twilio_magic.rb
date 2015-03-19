ccb52299ae0b229c252761391e83da6ee972351d
ohhheyyyy <ashley.hartt@gmail.com>
2015-03-19 13:22:43 -0400

    Accept question through terminal, text response via Twilio
    Created list of possible answers to user input question; connected
    Twilio API such that response is texted to user.

--- twilio_magic.rb
+++ twilio_magic.rb
@@ -10,12 +10,62 @@ auth_token = "your auth_token"
 
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-# text myself a message
+# acquire user question; store question as variable
+
+puts "I am the Ruby Magic 8 Ball; please ask me your question."
+
+user_question = gets
+
+# create list of possible answers
+
+answer_list = [
+	"It is certain",
+
+	"It is decidedly so",
+
+	"Without a doubt",
+
+	"Yes definitely",
+
+	"You may rely on it",
+
+	"As I see it, yes",
+
+	"Most likely",
+
+	"Outlook good",
+
+	"Yes",
+
+	"Signs point to yes",
+
+	"Reply hazy try again",
+
+	"Ask again later",
+
+	"Better not tell you now",
+
+	"Cannot predict now",
+
+	"Concentrate and ask again",
+
+	"Don't count on it",
+
+	"My reply is no",
+
+	"My sources say no",
+
+	"Outlook not so good",
+
+	"Very doubtful"
+]
+
+# text myself an answer to the question
 
 message = @client.account.messages.create(
-  :from => "+your twilio number",
-  :to =>"+your real number",
-  :body => "Haaaii!"
+  :from => "+ your Twilio number",
+  :to =>"+ your real number",
+  :body => answer_list.sample
 )
  
 puts message.to
