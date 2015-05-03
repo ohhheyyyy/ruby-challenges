3e5a9266c9a96980c3368865344ad851e33705a3
ohhheyyyy <ashley.hartt@gmail.com>
2015-05-02 23:15:30 -0400

    Create parent / child classes as refresher

--- oop2.rb
+++ oop2.rb
@@ -15,7 +15,7 @@ class Plants
 	end
 
 	def get_sunlight_hours
-		return @sunlight_hours
+		return sunlight_hours
 	end
 end
 
@@ -37,34 +37,36 @@ class Garden < Plants
 		return @season
 	end
 
-	def about_garden 
-		return "I am growing a #{@name} plant and it requires 
-		#{@sunlight_hours} of sunlight each day to grow. It is 
+	def about_garden
+		return "I am growing a #{@name} plant and it requires
+		#{@sunlight_hours} of sunlight each day to grow. It is
 		best grown in the #{@season}."
 	end
-end 
+end
 
-class Tree < Plants
+
+class Trees < Plants
 
 	def set_height=(height)
-		@height = height		
+		@height = height
 	end
 
 	def get_height
 		return @height
 	end
 
-	def about_tree_fir
-		return "Another plant I grow is a #{@name} tree; it can grow to be #{@height} tall."
-	end	
+	def about_tree
+		return "Another plant I grow is a #{@name} tree; it can
+		grow to be #{@height} tall."
+	end
 end
 
 my_garden_tomato = Garden.new
 my_garden_tomato.set_name = 'tomato'
 my_garden_tomato.set_sunlight_hours = '7 hours'
 my_garden_tomato.set_season = 'summer'
-my_tree_fir = Tree.new
-my_tree_fir.set_name = 'fir'
-my_tree_fir.set_height = '20ft'
+my_tree_fir = Trees.new
+my_tree_fir.set_name = 'Fir'
+my_tree_fir.set_height = '20 feet'
 puts my_garden_tomato.about_garden
-puts my_tree_fir.about_tree_fir
+puts my_tree_fir.about_tree
